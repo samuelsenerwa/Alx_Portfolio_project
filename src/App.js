72c92828dae73d5a4607924553652debfb8b996f
senerwa <samuelsenerwa>
2023-06-21 12:59:18 +0300

    containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Preloader } from './Components';
 
+
 function App() {
   return (
     <div className="App">
       <Preloader/>
         <p>Header</p>
+        <p>Navbar</p>
+        <p>ReactOwlCarousel</p>
+        
     </div>
   );
 }
