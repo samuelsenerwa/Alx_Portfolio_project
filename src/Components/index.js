a70a5530d13dea38ca9b3d4a08df551f8226797c
senerwa <samuelsenerwa>
2023-06-21 13:17:30 +0300

    files

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,10 +2,12 @@ import Preloader from './Preloader/Preloader';
 import Footer from './Footer/Footer';
 import Navbar from './Navbar/Navbar';
 import Slider from './Slider/Slider';
+import Map from './Map/Map';
 
 export {
     Preloader,
     Footer,
     Navbar,
-    Slider
+    Slider,
+    Map
 };
---
+++ src/Container/index.js
@@ -0,0 +1,13 @@
+import Appointment from './Appointment/Appointment';
+import Header from './Header/Header';
+import Home from './Home/Home';
+import News from './News/News';
+import Team from './Team/Team';
+
+export {
+    Appointment,
+    Header,
+    Home,
+    News,
+    Team
+};
