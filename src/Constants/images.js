7c4032853526ca1520adf5aaec9bad63db8fa292
senerwa <samuelsenerwa>
2023-06-21 12:21:00 +0300

    constants

---
+++ src/Constants/images.js
@@ -0,0 +1,44 @@
+import aboutBg from '../Images/about-bg.jpg';
+import appointment from '../Images/appointment-image.jpg';
+import author from '../Images/author-image.jpg';
+import slider1 from '../Images/slider1.jpg';
+import slider2 from '../Images/slider2.jpg';
+import slider3 from '../Images/slider3.jpg';
+import team1 from '../Images/team-image1.jpg';
+import team2 from '../Images/team-image2.jpg';
+import team3 from '../Images/team-image3.jpg';
+import news from '../Images/news-image.jpg';
+import news1 from '../Images/news-image1.jpg';
+import news2 from '../Images/news-image2.jpg';
+import news3 from '../Images/news-image3.jpg';
+import medicare1 from '../Images/medicare1.jpg';
+import medicare2 from '../Images/medicare2.jpg';
+import medicare3 from '../Images/medicare3.jpg';
+import medicare4 from '../Images/medicare4.jpg';
+import medicare5 from '../Images/medicare5.jpg';
+import medicare6 from '../Images/medicare6.jpg';
+import medicare7 from '../Images/medicare7.jpg';
+
+
+export default {
+    aboutBg,
+    appointment,
+    author,
+    slider1,
+    slider2,
+    slider3,
+    team1,
+    team2,
+    team3,
+    news,
+    news1,
+    news2,
+    news3,
+    medicare1,
+    medicare2,
+    medicare3,
+    medicare4,
+    medicare5,
+    medicare6,
+    medicare7
+};
