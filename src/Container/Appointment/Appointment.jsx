563ffd23d08a03b62d03e9d94ddc94d9fe751874
senerwa <samuelsenerwa>
2023-06-22 23:09:10 +0300

    form-control

--- src/Container/Appointment/Appointment.jsx
+++ src/Container/Appointment/Appointment.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React from 'react';
+import {Preloader} from '../../Components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import images from '../../Constants/images';
+import './Appointment.css';
 
-function Appointment() {
+const Appointment = () => {
   return (
-    <div>Appointment</div>
+     <>
+     <Preloader/>
+    <section id="appointment" data-stellar-background-ratio="3">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-6 col-sm-6">
+                         <img src={images.medicare3} class="img-responsive" alt="" />
+                    </div>
+
+                    <div class="col-md-6 col-sm-6">
+                        
+                         <form id="appointment-form" role="form" method="post" action="#">
+                              <div class="section-title wow fadeInUp" data-wow-delay="0.4s">
+                                   <h2>Make an appointment</h2>
+                              </div>
+
+                              <div class="wow fadeInUp" data-wow-delay="0.8s">
+                                   <div class="col-md-6 col-sm-6">
+                                        <label for="name">Name</label>
+                                        <input type="text" class="form-control" id="name" name="name" placeholder="Full Name"/>
+                                   </div>
+
+                                   <div class="col-md-6 col-sm-6">
+                                        <label for="email">Email</label>
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Your Email"/>
+                                   </div>
+
+                                   <div class="col-md-6 col-sm-6">
+                                        <label for="date">Select Date</label>
+                                        <input type="date" name="date" value="" class="form-control"/>
+                                   </div>
+
+                                   <div class="col-md-6 col-sm-6">
+                                        <label for="select">Select Department</label>
+                                        <select class="form-control">
+                                             <option>General Health</option>
+                                             <option>Cardiology</option>
+                                             <option>Dental</option>
+                                             <option>Medical Research</option>
+                                        </select>
+                                   </div>
+
+                                   <div class="col-md-12 col-sm-12">
+                                        <label for="telephone">Phone Number</label>
+                                        <input type="tel" class="form-control" id="phone" name="phone" placeholder="Phone"/>
+                                        <label for="Message">Additional Message</label>
+                                        <textarea class="form-control" rows="5" id="message" name="message" placeholder="Message"></textarea>
+                                        <button type="submit" class="form-control" id="cf-submit" name="submit">Submit Button</button>
+                                   </div>
+                              </div>
+                        </form>
+                    </div>
+
+               </div>
+          </div>
+     </section>
+     </>
   )
 }
 
