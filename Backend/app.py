ca2d9394a844c0714257e785af9a24dabe22573e
senerwa <samuelsenerwa>
2023-06-26 22:14:50 +0300

    minor fix

--- Backend/app.py
+++ Backend/app.py
@@ -1,37 +1,3 @@
-# from flask import Flask, request
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-# CORS(app)
-
-
-# @app.route("/appointment", methods=["POST"])
-# def handle_appointment():
-#     data = request.get_json()
-
-#     name = request.form.get("name")
-#     email = request.form.get("email")
-#     date = request.form.get("date")
-#     department = request.form.get("department")
-#     phone = request.form.get("phone")
-#     message = request.form.get("message")
-
-#     # Process the form data and perform necessary operations
-
-#     print(f"Name: {name}")
-#     print(f"Email: {email}")
-#     print(f"Date: {date}")
-#     print(f"Department: {department}")
-#     print(f"Phone: {phone}")
-#     print(f"Message: {message}")
-
-#     return "Appointment submitted successfully"
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 from flask import Flask, request, g, jsonify
 from flask_cors import CORS
 import sqlite3
