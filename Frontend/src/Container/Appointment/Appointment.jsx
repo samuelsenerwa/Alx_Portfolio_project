9df64d87a6d946001103502118513b81358a38dd
senerwa <samuelsenerwa>
2023-06-26 22:00:57 +0300

    final form validation

--- Frontend/src/Container/Appointment/Appointment.jsx
+++ Frontend/src/Container/Appointment/Appointment.jsx
@@ -20,10 +20,26 @@ const Appointment = () => {
   const [department, setDepartment] = useState('General Health');
   const [phone, setPhone] = useState('');
   const [message, setMessage] = useState('');
-
+  const [errors, setErrors] = useState({})
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    //form validation to ensure no details is missing
+    const validationErrors = {};
+    if (!name) {
+      validationErrors.name = 'Name is required';
+    }
+
+    if (!email) {
+      validationErrors.email = 'Email is required';
+    }
+
+    //if no validation errors
+    if(Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
   
     try {
       const formData = {
@@ -33,8 +49,7 @@ const Appointment = () => {
         department: department,
         phone: phone,
         message: message,
-      };
-  
+      }  
       const serializedFormData = JSON.stringify(formData);
       const response = await axios.post('http://127.0.0.1:5000/appointment', serializedFormData, {
         headers: {
@@ -42,7 +57,15 @@ const Appointment = () => {
         }
       });
 
-  
+      //clearing the input fileds after successful submission
+      setName('');
+      setEmail('');
+      setSelectedDate('');
+      setDepartment('General Health');
+      setPhone('');
+      setMessage('');
+
+      //sweet alert popup
       swal('Success', response.data, 'success');
     } catch (error) {
       console.error(error);
@@ -77,14 +100,30 @@ const Appointment = () => {
                   <div className="col-md-6 col-sm-6">
                     <div className="form-group">
                       <label htmlFor="name">Name</label>
-                      <input type="text" className="form-control" id="name" name="name" placeholder="Full Name" value={name} onChange={(e) => setName(e.target.value)} />
+                      <input type="text" 
+                      className={`form-control ${errors.name ? 'is-invalid': ''}`} 
+                      id="name" 
+                      name="name" 
+                      placeholder="Full Name" 
+                      value={name} 
+                      onChange={(e) => setName(e.target.value)} />
+                      {
+                        errors.name &&
+                        <div className='invalid-feedback'>{errors.name}</div>
+                      }
                     </div>
                   </div>
 
                   <div className="col-md-6 col-sm-6">
                     <div className="form-group">
                       <label htmlFor="email">Email</label>
-                      <input type="email" className="form-control" id="email" name="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                      <input type="email" 
+                      className={`form-control ${errors.email ? 'is-invalid': ''}`} 
+                      id="email" 
+                      name="email" 
+                      placeholder="Your Email" 
+                      value={email} onChange={(e) => setEmail(e.target.value)} />
+                      {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
                     </div>
                   </div>
 
