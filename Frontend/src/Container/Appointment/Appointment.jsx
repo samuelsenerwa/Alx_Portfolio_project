f21a031399b0a70ebed97a84dfbb469a09c7cd26
senerwa <samuelsenerwa>
2023-06-26 15:56:42 +0300

    fix

--- Frontend/src/Container/Appointment/Appointment.jsx
+++ Frontend/src/Container/Appointment/Appointment.jsx
@@ -1,40 +1,10 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Swal from 'sweetalert2';
+import React from 'react';
 import {Preloader} from '../../Components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import images from '../../Constants/images';
 import './Appointment.css';
 
 const Appointment = () => {
-  
-  const[formData, setFormData] = useState({
-    name:'',
-    email: '',
-    date: '',
-    department: 'General Helath',
-    phone: '',
-    message:'',
-  });
-
-  const handleChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    axios
-    .post('/api/appointment', formData)
-    .then((response) => {
-      Swal.DismissReason('Success', 'Appointment booked Successfully', 'success');
-    })
-    .catch((error) => {
-      Swal.fire('Error', 'An error occured', 'error');
-      console.log(error);
-    });
-  };
-
   return (
      <>
      <Preloader/>
@@ -48,7 +18,7 @@ const Appointment = () => {
 
           <div className="col-md-6 col-sm-6">
             {/* CONTACT FORM HERE */}
-            <form id="appointment-form" role="form"  action="#" onSubmit={handleSubmit}>
+            <form id="appointment-form" role="form" method="post" action="#">
 
               {/* SECTION TITLE */}
               <div className="section-title wow fadeInUp" data-wow-delay="0.4s">
@@ -60,7 +30,7 @@ const Appointment = () => {
                   <div className="col-md-6 col-sm-6">
                     <div className="form-group">
                       <label htmlFor="name">Name</label>
-                      <input type="text" className="form-control" id="name" name="name" placeholder="Full Name" value={formData.name} onChange={handleChange}/>
+                      <input type="text" className="form-control" id="name" name="name" placeholder="Full Name" />
                     </div>
                   </div>
 
@@ -97,7 +67,7 @@ const Appointment = () => {
                     </div>
                     <div className="form-group">
                       <label htmlFor="Message">Additional Message</label>
-                      <textarea className="form-control" rows="5" id="message" name="message" placeholder="Message" value={formData.message} onChange={handleChange}></textarea>
+                      <textarea className="form-control" rows="5" id="message" name="message" placeholder="Message"></textarea>
                     </div>
                     <div className="form-group">
                       <button type="submit" className="form-control" id="cf-submit" name="submit">Submit Button</button>
