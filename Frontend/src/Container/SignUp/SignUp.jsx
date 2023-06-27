b29a3dd1486d680be4650752904d68f184f1d957
samuelsenerwa <samuelsenerwa@gmail.com>
2023-06-27 16:22:49 +0300

    signup

--- Frontend/src/Container/SignUp/SignUp.jsx
+++ Frontend/src/Container/SignUp/SignUp.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import images from '../../Constants/images';
+import './SignUp.css'
 
-const SignUp = () => {
+function SignUp() {
   return (
-    <section className='sign-up'/>
-  );
-};
+    <div className="sign-up  section__padding">
+    <div className="sign-up-left">
+      <img src={images.medicare4} alt="chef_image"/>
+    </div>
+    
+    <div className="app__wrapper_info">
+      <h1 className="headtext__cormorant">What We Believe In</h1>
 
-export default SignUp;
+      <div className="sign-up-right">
+        </div>
+    </div> 
+  </div>
+  );
+}
+
+export default SignUp
