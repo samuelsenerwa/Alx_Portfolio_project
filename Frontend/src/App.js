62e053a17f701de0aac4e47035824a72e14b57d2
samuelsenerwa <samuelsenerwa@gmail.com>
2023-06-29 16:04:23 +0300

    fix

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,36 +7,33 @@ import NewsDetail from './Container/NewsDetail/NewsDetail';
 
 
 const App = () => {
-  // const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-  // const handleSignUp = (userData) => {
-  //   setUser(userData);
-  // };
+  const handleSignUp = (userData) => {
+    setUser(userData);
+  };
 
   return (
-    // <div>
-    //   {user ? (
-    //     <div>
-    //       <Preloader />
-    //       <Navbar />
-    //       <Routes>
-    //         <Route path="/" element={<Home />} />
-    //         <Route path="/about" element={<AboutUs />} />
-    //         <Route path="/team" element={<Team />} />
-    //         <Route path="/news" element={<News />} />
-    //         <Route path="/contact" element={<Contact />} />
-    //         <Route path="/appointment" element={<Appointment />} />
-    //         {/* <Route path="/news-detail" element={<NewsDetail />} /> */}
-    //         {/* <Route path="/news-detail/:index" element={<NewsDetail />} /> */}
-    //       </Routes>
-    //       <Footer/>
-    //     </div>
-    //   ) : (
-    //     <SignUp onSignUp={handleSignUp} />
-    //   )}
-    // </div>
     <div>
-      <Team/>
+      {user ? (
+        <div>
+          <Preloader />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/appointment" element={<Appointment />} />
+            {/* <Route path="/news-detail" element={<NewsDetail />} /> */}
+            {/* <Route path="/news-detail/:index" element={<NewsDetail />} /> */}
+          </Routes>
+          <Footer/>
+        </div>
+      ) : (
+        <SignUp onSignUp={handleSignUp} />
+      )}
     </div>
   );
 };
