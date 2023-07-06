954042bb87e43ae9ddadb4b0a710d6b4b0ddb782
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-06 12:23:19 +0300

    api call fixed

--- Frontend/src/Container/News/News.jsx
+++ Frontend/src/Container/News/News.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import images from '../../Constants/images';
 import { Preloader } from '../../Components';
 import './News.css';
 
@@ -11,12 +10,9 @@ const News = () => {
   useEffect(() => {
     const fetchHealthNews = async () => {
       try {
-        const response = await axios.get('https://newsapi.org/v2/top-headlines?category=health&country=us&apiKey=96d8094b9d584e4baa94603f7125144b', {
-  httpVersion: 'HTTP/1.1'
-});
+        const response = await axios.get('https://newsdata.io/api/1/news?apikey=pub_25812fc32ee4cffc907d4878ddf91ab590a72&category=health&language=en');
 
-
-        setNews(response.data.articles);
+        setNews(response.data.results);
         setLoading(false);
       } catch (error) {
         console.log(error);
@@ -39,24 +35,17 @@ const News = () => {
               {news.map((article, index) => (
                 <div className="col-md-4 col-sm-6" key={index}>
                   <div className="news-thumb wow fadeInUp" data-wow-delay={`${index * 0.2}s`}>
-                    <a href={article.url} target="_blank" rel="noopener noreferrer">
-                      <img src={article.urlToImage} id="img-responsive" alt="" />
+                    <a href={article.link} target="_blank" rel="noopener noreferrer">
+                      <img src={article.image_url} id="img-responsive" alt="" />
                     </a>
                     <div className="news-info">
-                      <span>{article.publishedAt}</span>
+                      <span>{article.pubDate}</span>
                       <h3>
-                        <a href={article.url} target="_blank" rel="noopener noreferrer">
+                        <a href={article.link} target="_blank" rel="noopener noreferrer">
                           {article.title}
                         </a>
                       </h3>
                       <p>{article.description}</p>
-                      <div className="author border-top">
-                        <img src={images.author} className="img-responsive" alt="" />
-                        <div className="author-info">
-                          <h5>{article.author}</h5>
-                          <p>{article.source.name}</p>
-                        </div>
-                      </div>
                     </div>
                   </div>
                 </div>
