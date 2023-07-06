8a1cdf79ed7a1dbab0cfc5c9f8d9fe761c82c774
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-06 11:04:16 +0300

    build

---
+++ Frontend/build/precache-manifest.1cbcc953b3c460388e9b1a4c8cab4320.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1d268d3f9eea9a1f212",
+    "url": "/static/css/main.e1f94d2b.chunk.css"
+  },
+  {
+    "revision": "e1d268d3f9eea9a1f212",
+    "url": "/static/js/main.e1d268d3.chunk.js"
+  },
+  {
+    "revision": "f33ff011a69b00e9a76e",
+    "url": "/static/css/1.730e4777.chunk.css"
+  },
+  {
+    "revision": "f33ff011a69b00e9a76e",
+    "url": "/static/js/1.f33ff011.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0dfca84393a528cb22db4429870d20bc",
+    "url": "/static/media/about-bg.0dfca843.jpg"
+  },
+  {
+    "revision": "a0a821f03db855902966f787313c224e",
+    "url": "/static/media/appointment-image.a0a821f0.jpg"
+  },
+  {
+    "revision": "ed29ce5e19ba0ae3e2ba0e963c322a81",
+    "url": "/static/media/slider1.ed29ce5e.jpg"
+  },
+  {
+    "revision": "bbe8d37a2acb2c1a217b7d2a0210d43e",
+    "url": "/static/media/slider2.bbe8d37a.jpg"
+  },
+  {
+    "revision": "2a55a72acdabd8bc9b43dd01065b0206",
+    "url": "/static/media/slider3.2a55a72a.jpg"
+  },
+  {
+    "revision": "e8ba23574945febe5b326d968a184c57",
+    "url": "/static/media/team-image1.e8ba2357.jpg"
+  },
+  {
+    "revision": "e81efefd591a487d84159a94ac6b9e84",
+    "url": "/static/media/team-image2.e81efefd.jpg"
+  },
+  {
+    "revision": "998a897affcbc97adf6f1f23d5b5eb68",
+    "url": "/static/media/team-image3.998a897a.jpg"
+  },
+  {
+    "revision": "af8d8a1bbc782351335f0cb9e0f09468",
+    "url": "/static/media/news-image.af8d8a1b.jpg"
+  },
+  {
+    "revision": "5d981f22c8f86cc67e168d659f2c5595",
+    "url": "/static/media/news-image1.5d981f22.jpg"
+  },
+  {
+    "revision": "1d1fa39fe534a99b42e1aa32bc14ee92",
+    "url": "/static/media/news-image2.1d1fa39f.jpg"
+  },
+  {
+    "revision": "80e23630de9a4c31ac756b7fd6732495",
+    "url": "/static/media/news-image3.80e23630.jpg"
+  },
+  {
+    "revision": "0d89f37b7cb15143b73a4a8efadb439b",
+    "url": "/static/media/medicare1.0d89f37b.jpg"
+  },
+  {
+    "revision": "3da8657172a8106902bc0c04b4284aa4",
+    "url": "/static/media/medicare2.3da86571.jpg"
+  },
+  {
+    "revision": "307a6e02bcc6f703e3959341dbb2b192",
+    "url": "/static/media/medicare3.307a6e02.jpg"
+  },
+  {
+    "revision": "d8ca227a5ca1a7927c05e15ca79bcfc2",
+    "url": "/static/media/medicare4.d8ca227a.jpg"
+  },
+  {
+    "revision": "0af0b1358934772785be287fb5ac51da",
+    "url": "/static/media/medicare5.0af0b135.jpg"
+  },
+  {
+    "revision": "881183eccafe2d4710f7024e24116ae4",
+    "url": "/static/media/medicare6.881183ec.jpg"
+  },
+  {
+    "revision": "98fec57182a6e99e254f01ce3e91e56d",
+    "url": "/static/media/medicare7.98fec571.jpg"
+  },
+  {
+    "revision": "731e483c86136ab451d023f189b1bb1f",
+    "url": "/static/media/logo.731e483c.png"
+  },
+  {
+    "revision": "764093fd70eebdad31ae93ae5b9d9741",
+    "url": "/static/media/person.764093fd.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "39857bcbf81e351950d500eba392a7b1",
+    "url": "/index.html"
+  }
+];
