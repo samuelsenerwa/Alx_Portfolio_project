539c9bd3573c2d6abac86ef392cbe0561b65025f
GiddyLesGid <gideonlangat@gmail.com>
2023-07-12 07:48:09 -0700

    documenting App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,10 +5,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NewsDetail from './Container/NewsDetail/NewsDetail';
 
-
 const App = () => {
+  // User state to track the signed-in user
   const [user, setUser] = useState(null);
 
+  // Function to handle the sign-up process and update the user state
   const handleSignUp = (userData) => {
     setUser(userData);
   };
@@ -16,6 +17,7 @@ const App = () => {
   return (
     <div>
       {user ? (
+        // If user is signed in, render main sections of the application
         <div>
           <Preloader />
           <Navbar />
@@ -29,9 +31,10 @@ const App = () => {
             {/* <Route path="/news-detail" element={<NewsDetail />} /> */}
             {/* <Route path="/news-detail/:index" element={<NewsDetail />} /> */}
           </Routes>
-          <Footer/>
+          <Footer />
         </div>
       ) : (
+        // If user is not signed in, render the sign-up form
         <SignUp onSignUp={handleSignUp} />
       )}
     </div>
@@ -39,3 +42,4 @@ const App = () => {
 };
 
 export default App;
+
