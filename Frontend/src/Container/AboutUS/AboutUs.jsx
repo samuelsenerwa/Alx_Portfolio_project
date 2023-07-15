c4bfc46a298a3581c19dfd46e2be4791b9071586
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-15 12:43:20 +0300

    clean up

--- Frontend/src/Container/AboutUS/AboutUs.jsx
+++ Frontend/src/Container/AboutUS/AboutUs.jsx
@@ -15,14 +15,14 @@ function AboutUs() {
                    <div class="about-info">
                         <h2 className="wow animate_fadeInUp" data-wow-delay="0.5s">Welcome to <img src={images.logo}/>are-Bridge</h2>
                         <div class="wow fadeInUp" data-wow-delay="0.8s">
-                             <p>A place dedicated to your overall well-being. Our team of professionals is committed to providing personalized care and support that caters to your specific health needs..</p>
-                             <p>From preventive screenings to advanced treatments, we strive to empower you with the knowledge and resources necessary to achieve optimal health. Trust us to be your partner on your wellness journey, offering compassionate care in a welcoming environment.</p>
+                             <p>Our goal is to connect patients with doctors seamlessly through a digital platform, revolutionizing the healthcare experience.</p>
+                             <p> We tackle challenges like long wait times, limited specialist access, and manual appointment scheduling. Join us for a convenient and efficient healthcare journey!</p>
                         </div>
                         <figure class="profile wow fadeInUp" data-wow-delay="1s">
                              <img src={images.author} class="img-responsive" alt=""/>
                              <figcaption>
                                   <h3>Dr. Neil Jackson</h3>
-                                  <p>General Principal</p>
+                                  <p>Surgeon</p>
                              </figcaption>
                         </figure>
                    </div>
